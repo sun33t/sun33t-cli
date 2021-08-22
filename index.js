4494cb44b3194c4d90909188c2c61ba557da7945
Suneet Misra <hello@suneet.codes>
2021-08-22 23:38:20 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+console.log(`
+Suneet Misra
+
+Hi! I'm a nodeJS developer from Liverpool, UK. You can find me at:
+
+💻  Site:      https://suneet.codes
+📖  GitHub:   https://github.com/sun33t
+💼  LinkedIn: https://linkedin.com/in/suneetmisra
+
+`);
