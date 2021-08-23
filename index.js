70373f4399110ec6f3287a860fef9b6f02b3f9ef
Suneet Misra <hello@suneet.codes>
2021-08-23 01:39:07 +0100

    :sparkles: Colors
    Added coloured and styled text in the developer output

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 const welcome = require("cli-welcome");
+const chalk = require("chalk");
 const pkgJson = require("./package.json");
+const log = console.log;
+
+const blogColor = chalk.hex("#6cc644").bold.inverse;
+const workColor = chalk.hex("#c9510c").bold.inverse;
+const githubColor = chalk.hex("#6e5494").bold.inverse;
+const linkedinColor = chalk.hex("#0077b5").bold.inverse;
+const url = chalk.dim.underline.green;
 
 welcome({
   title: pkgJson.name,
-  tagLine: "😀",
   version: pkgJson.version,
   description: pkgJson.description,
   color: "#000000",
@@ -13,16 +20,17 @@ welcome({
   clear: true,
 });
 
-console.log(`
-Suneet Misra - nodeJS Engineer
+log(`${chalk.blue.bold(`Suneet Misra - Software Engineer`)}`);
+log(
+  `${chalk.italic(`
+Hi! I'm a full stack developer from Liverpool, UK. I like building things with JavaScript and TypeScript and automating tasks by creating CLIs.
 
-Hi! I'm a node developer from Liverpool, UK. I like building things with JavaScript and TypeScript and automating tasks by creating CLIs.
+Within my current role I'm using Node, TypeScript, React, NextJS, TDD, Storybook, Figma, mono-repo architecture and paired programming. I love collaboration and consider myself a life-long learner with a growth mindset which helps to keep me curious. I love sharing my knowledge with my colleagues as well as learning new things from them in turn! If you're interested in connecting with me, you can find me at:`)}`
+);
+log(`
 
-Within my current role I'm using Node, TypeScript, React, NextJS, TDD, Storybook, Figma, mono-repo architecture and paired programming. I love collaboration and consider myself a life-long learner with a growth mindset which helps to keep me curious. I love sharing my knowledge with my colleagues as well as learning new things from them in turn! If you're interested in connecting with me, you can find me at:
-
-💻  Blog:     https://suneet.codes
-🧰  Work:     https://verse.co.uk
-📖  GitHub:   https://github.com/sun33t
-💼  LinkedIn: https://linkedin.com/in/suneetmisra
-
+${blogColor(` Blog     `)}  ${url(`https://suneet.codes`)}
+${workColor(` Work     `)}  ${url(`https://verse.co.uk`)}
+${githubColor(` GitHub   `)}  ${url(`https://github.com/sun33t`)}
+${linkedinColor(` LinkedIn `)}  ${url(`https://linkedin.com/in/suneetmisra`)}
 `);
