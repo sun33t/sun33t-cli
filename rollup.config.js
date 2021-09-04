067d49799cae0a35473d31b15c692d89f45982b4
Suneet Misra <hello@suneet.codes>
2021-09-04 21:02:28 +0100

    :wrench: rollup config
    Added a rollup configuration to output production JS

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import del from 'rollup-plugin-delete';
+import typescript from 'rollup-plugin-typescript2';
+import shebang from 'rollup-plugin-preserve-shebang';
+import copy from 'rollup-plugin-copy';
+import json from '@rollup/plugin-json';
+import pkg from './package.json';
+
+const extensions = ['js', 'json', 'ts'];
+
+const externals = [
+	...Object.keys(pkg.dependencies || {}),
+	...Object.keys(pkg.devDependencies || {}),
+	...Object.keys(pkg.peerDependencies || {}),
+];
+
+const config = {
+	input: 'index.ts',
+	output: {
+		file: pkg.main,
+		format: 'esm',
+	},
+	external: externals,
+	plugins: [
+		del({targets: ['dist']}),
+		shebang(),
+		json(),
+		nodeResolve({extensions}),
+		commonjs(),
+		typescript({
+			tsconfigOverride: {
+				exclude: ['node_modules'],
+			},
+		}),
+		copy({
+			targets: [
+				{src: 'README.md', dest: 'dist'},
+				{src: 'LICENSE', dest: 'dist'},
+			],
+		}),
+	],
+};
+
+export default config;
